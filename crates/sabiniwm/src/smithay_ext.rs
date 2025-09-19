5ab38662cc4c39e213f88b95e617314c58a1e406
kenoss <keno.ss57@gmail.com>
2025-09-19 12:47:09 +0900

    refactor: Improve comment/message for panic

--- crates/sabiniwm/src/smithay_ext.rs
+++ crates/sabiniwm/src/smithay_ext.rs
@@ -1,13 +1,17 @@
 use smithay::utils::{Logical, Size};
 
 pub(crate) trait OutputExt {
+    // Panics: Panics if `Output::current_mode()` is `None`.
     fn current_logical_size(&self) -> Size<i32, Logical>;
 }
 
 impl OutputExt for smithay::output::Output {
     fn current_logical_size(&self) -> Size<i32, Logical> {
         let transform = self.current_transform();
-        let size = self.current_mode().unwrap().size;
+        let size = self
+            .current_mode()
+            .expect("Output::current_mode() is set")
+            .size;
         let scale = self.current_scale().fractional_scale();
 
         transform
